0a6738efbf99d242050ec3ea827e023b39b82235
Jongwan93 <jwhur1993@gmail.com>
2024-06-05 22:31:06 -0400

    minor fix

--- Program.cs
+++ Program.cs
@@ -91,34 +91,34 @@ namespace ChampSelectNameFinder
             }
         }
 
-static void OpenUrl(string url)
-{
-    try
-    {
-        Process.Start(url);
-    }
-    catch
-    {
-        // hack because of this: https://github.com/dotnet/corefx/issues/10361
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        static void OpenUrl(string url)
         {
-            url = url.Replace("&", "^&");
-            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        {
-            Process.Start("xdg-open", url);
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-        {
-            Process.Start("open", url);
-        }
-        else
-        {
-            throw;
-        }
-    }
-}
 
         static string GetOPGG(string req)
         {
--- obj/Debug/net7.0/champSelectNameFinder.AssemblyInfo.cs
+++ obj/Debug/net7.0/champSelectNameFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("champSelectNameFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b826420f3f9253f403fa3f2800b951d6c5615c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("champSelectNameFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("champSelectNameFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
